9a901f8300d8e66e2e58e07273e201e0cafc7eee
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-04-24 11:30:43 +0300

    lists update

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,13 +2,16 @@ import React from "react";
 
 function ProjectItem({ about, technologies, name }) {
   console.log(name);
+  const newTech = technologies.map((tech) => {
+    <span key={tech}>{tech}</span>;
+  });
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
-        <span>{technologies}</span>
+        {newTech}
       </div>
     </div>
   );
